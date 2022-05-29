6c82c0f764cbd6d1b0cbdac5225a10b6487d03ee
Kalle Tolonen <kallet@debian-BULLSEYE-live-builder-AMD64>
2022-05-29 18:39:03 +0300

    Add BookInstance & Loan info.

--- library/booksofmine/models.py
+++ library/booksofmine/models.py
@@ -1,16 +1,39 @@
 from django.db import models
+from datetime import datetime, timedelta
+from django.contrib.auth.models import User
 
 class Book(models.Model):
     title = models.CharField(max_length=3000)
-    author = models.ForeignKey('Author', on_delete=models.CASCADE)
-    genre = models.ForeignKey('Genre', on_delete=models.CASCADE)
+    author = models.ForeignKey('Author', null=True, on_delete=models.SET_NULL)
+    genre = models.ForeignKey('Genre', null=True,on_delete=models.SET_NULL)
 
     def __str__(self):
             return f"{self.title}"
 
     def get_absolute_url(self):
         return f"/books/{self.pk}" 
-            
+
+class BookInstance(models.Model):
+    def get_duedate():
+        return datetime.today() + timedelta(days=30)
+
+    user = models.ForeignKey('auth.User',null=True, on_delete=models.SET_NULL)
+    book = models.ForeignKey('Book', null=True,on_delete=models.SET_NULL)
+    loan_date = models.DateField(null=True, blank=True, default=datetime.today)
+    due_back = models.DateField(null=True, blank=True, default=get_duedate)
+    LOAN_STATUS = (
+            ('o', 'On loan'),
+            ('a', 'Available'),
+            ('r', 'Reserved'),
+        )
+    status = models.CharField(
+            max_length=1,
+            choices=LOAN_STATUS,
+            blank=True,
+            default='o',
+            help_text='Book availability',
+        )
+           
 class Author(models.Model):
     name = models.CharField(max_length=300, default="")
     country = models.CharField(max_length=300, null=True, blank=True)
@@ -32,3 +55,4 @@ class Genre(models.Model):
         return f"/genres/{self.pk}" 
 
     
+
